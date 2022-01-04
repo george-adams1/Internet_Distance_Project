4e987ce1b2cebd261ce7ae55d4c6d2c0e1c2757f
patelmeet761 <60280230+patelmeet761@users.noreply.github.com>
2022-01-04 16:34:13 -0500

    TotalDistance

--- background.js
+++ background.js
@@ -1,12 +1,32 @@
 chrome.runtime.onInstalled.addListener(({ reason, version }) => {
     if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
       showReadme();
-      window.localStorage.setItem("distance", 0);
+
+      var initialDistance = 0;
+      chrome.storage.sync.set({key: totalDistanceTraveled}, function() {
+        console.log('On install, value is set to ' + initialDistance);
+      }); 
+    }
+  })
+
+  chrome.tabs.onUpdated.addListener(function (id, info, tab) {
+    if (tab.status === 'loading') {
+
+
+      fetch("https://ipinfo.io/json?d06fb76a59e1a7")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+
+      });
 
     }
-  });
+  })
+
 
   function showReadme(info, tab) {
     let url = chrome.runtime.getURL("readme.html");
     chrome.tabs.create({ url });
   }
+
+
--- popup.js
+++ popup.js
@@ -2,63 +2,74 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var tab = tabs[0];
   var url = new URL(tab.url);
   var domain = url.hostname; // `domain` now has a value like 'example.com'
+  console.log(url);
 
   // display current domain
   var domainUI = document.getElementById("userDomain");
-  domainUI.innerText = domain;
+
 
   var ipAddressUI = document.getElementById("userIp");
   var locationUI = document.getElementById("userLocation");
 
+  var serverIpUI = document.getElementById("serverIp");
+  var distanceUI = document.getElementById("distance");
+
   var totalDistanceUI = document.getElementById("totalDistance");
-
-
-  var totalDistanceTraveled = 0;
 
   // fetch user ip info
   fetch("https://ipinfo.io/json?d06fb76a59e1a7")
     .then((res) => res.json())
     .then((data) => {
-       console.info(JSON.stringify(data));
+      console.info(JSON.stringify(data));
 
       // display user ip address
       var ip = data.ip;
-      console.log(ip);
-      ipAddressUI.innerText = ip;
+     // console.log(ip);
      
+
       var city = data.city;
       var region = data.region;
       var country = data.country;
 
       var location = city + ", " + region + ", " + country;
-        locationUI.innerText = location;
-
-
-        // NEED TO WORK ON TOTAL DISTANCE ACCUMULATED
-        // hardcoded for reference
-        var distance = 100;
-        totalDistanceTraveled += distance;
-       // totalDistanceUI.innerText = totalDistanceTraveled;
-
-
-       let totalDistance = parseInt(window.localStorage.getItem("distance"));
-       console.log(totalDistance);
-
-       totalDistance += distance;
-
-       console.log(totalDistance);
-
-       window.localStorage.setItem("distance", totalDistance);
      
-      
 
-        
+      var totalDistanceTraveled = 0;
 
-        // RETRIEVE SERVER IP, LOCATION, AND CALCULATE DISTANCE
+      chrome.storage.sync.get(['key'], function(result) {
+        console.log('Value currently is ' + result.key);
+        totalDistanceTraveled  = result.key;
+      });
 
-        // ADDTIONAL FEATURE: ADD A MAP
+      // https://internet-distance.herokuapp.com/IP/24.50.91.241/apple.com
+
+      fetch(`https://internet-distance.herokuapp.com/IP/${ip}/${domain}`)
+        .then((res) => res.json())
+        .then((data) => {
+
+         // console.info(JSON.stringify(data));
+          var serverIp = data.server_IP;
+          var distance = data.distance;
+          
+          var newTotalDistanceTraveled = parseFloat(totalDistanceTraveled + distance).toFixed(2);
+          
+          let conversion = 0.621371;
+          var distanceInMiles = parseInt(distance) * conversion;
+          var totalDistance = parseFloat(newTotalDistanceTraveled);
+          var totalDistanceInMiles = parseFloat(totalDistance) * conversion;
+          
+          chrome.storage.sync.set({key: totalDistance}, function() {
+            console.log('Value is set to ' + totalDistance);
+          }); 
+    
+          domainUI.innerText = domain;
+          ipAddressUI.innerText = ip;
+          locationUI.innerText = location;
+          serverIpUI.innerText = serverIp;
+          distanceUI.innerText = `${distanceInMiles.toFixed(2)} mi / ${distance} km`;
+          totalDistanceUI.innerText = `${totalDistanceInMiles.toFixed(2)} mi / ${totalDistance.toFixed(2)} km`
+        });
+
+     
     });
-
-   
-
 });
